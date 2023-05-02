74cf1e432ed599f9a7cfb587402414edbdc1b414
Je Min Yun <jmy@macbook-pro.myfiosgateway.com>
2023-05-02 17:01:32 -0400

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -1,23 +1,73 @@
-////////////////////////////////
-// Easy Going
-////////////////////////////////
-/// Write a for loop that will log the numbers 1 through 20.
-// for (let i = 1; i <=20; i++) {
-//     console.log(i);
+// ////////////////////////////////
+// // Easy Going
+// ////////////////////////////////
+// /// Write a for loop that will log the numbers 1 through 20.
+// // for (let i = 1; i <=20; i++) {
+// //     console.log(i);
+// // }
+
+// ////////////////////////////////
+// // Get Even
+// ////////////////////////////////
+// ///// Write a for loop that will log only the even numbers in 0 through 200.
+// // for (let i = 0; i <= 200; i+= 2) {
+// //     console.log(i);
+// // }
+
+// ////////////////////////////////
+// // Fizz Buzz
+// ////////////////////////////////
+// // Write a javascript application that logs all numbers from 1 - 100.
+// function logNumbers() {
+//     for (let i = 1; i <= 100; i++) {
+//         console.log(i);
+//     }
 // }
 
-////////////////////////////////
-// Get Even
-////////////////////////////////
-///// Write a for loop that will log only the even numbers in 0 through 200.
-// for (let i = 0; i <= 200; i+= 2) {
-//     console.log(i);
+// logNumbers();
+
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// function logNumbers() {
+//     for (let i = 1; i <= 100; i++) {
+//         if (i % 3 === 0) {
+//             console.log("fizz");
+//         } else {
+//             console.log(i);
+//         }
+//     }
 // }
 
-////////////////////////////////
-// Fizz Buzz
-////////////////////////////////
+// logNumbers();
 
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// function logNumbers() {
+//     for (let i = 1; i <= 100; i++) {
+//         if (i % 5 === 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// logNumbers();
+
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+// function logNumbers() {
+//     for (let i = 1; i <= 100; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz")
+//         } else if (i % 3 === 0) {
+//             console.log("Fizz");
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// logNumbers();
 
 ////////////////////////////////
 // Wild Wild Life
